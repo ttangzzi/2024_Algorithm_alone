0eb43c9bcd62418210361cf93ce65c957391da22
ttangzzi <cvbg0802@gmail.com>
2024-05-10 15:03:01 +0900

    28278 시간초과

--- 스택/1874-스택 수열.py
+++ 스택/1874-스택 수열.py
@@ -1,20 +1,20 @@
-n = int(input())
-numList = [0]*n
-save = 1
+text = input()
 
-for i in range(n):
-  numList[i] = int(input())
-
-for i in range(n):
-  count = 1
-  while True:
-
-    if count-1 == numList[i]:
-      print("-")
-      break
-
-    elif count == save+1:
-      print("+")
-      save = count
-      count += 1
-    
+while text != '.':
+    stack = []
+    for char in text:
+        if char == '(' or char == ')':
+            stack.append(char)
+            if stack[-1] == '(' and stack[-2] == ')':
+                stack.pop()
+                stack.pop()
+        if char == '[' or char == ']':
+            stack.append(char)
+            if stack[-1] == '[' and stack[-2] == ']':
+                stack.pop()
+                stack.pop()
+    if len(stack) == 0:
+        print("yes")
+    else:
+        print("no")
+    text = input()
---
+++ 스택/28278-스택2.py
@@ -0,0 +1,31 @@
+# 1 X: 정수 X를 스택에 넣는다. (1 ≤ X ≤ 100,000) push
+# 2: 스택에 정수가 있다면 맨 위의 정수를 빼고 출력한다. 없다면 -1을 대신 출력한다. pop
+# 3: 스택에 들어있는 정수의 개수를 출력한다. len
+# 4: 스택이 비어있으면 1, 아니면 0을 출력한다. len = 0 ?
+# 5: 스택에 정수가 있다면 맨 위의 정수를 출력한다. 없다면 -1을 대신 출력한다. top
+N = int(input())
+stack = []
+
+for _ in range(N):
+  order = input().split()
+
+  if order[0] == '1':
+      stack.append(int(order[1]))
+  elif order[0] == '2':
+      if len(stack) == 0:
+          print(-1)
+      else:
+          print(stack.pop())
+      
+  elif order[0] == '3':
+      print(len(stack))
+  elif order[0] == '4':
+      if len(stack) == 0:
+          print(1)
+      else:
+          print(0)
+  elif order[0] == '5':
+      if len(stack) == 0:
+          print(-1)
+      else:
+        print(stack[-1])
