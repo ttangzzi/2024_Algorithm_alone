d0754cdfc5ed56d36ed526a55dc145bf7676583c
ttangzzi <cvbg0802@gmail.com>
2024-06-25 22:51:43 +0900

    0624 update

--- 문자열/-1157-단어 공부.py
+++ 문자열/-1157-단어 공부.py
@@ -10,3 +10,6 @@ if result.count(max(result)) > 1:
 
 else:
   print(word_list[result.index(max(result))])
+
+
+# 참고 : https://velog.io/@goplanit/Algorithm-%EB%B0%B1%EC%A4%80-1157%EB%B2%88-%EB%8B%A8%EC%96%B4-%EA%B3%B5%EB%B6%80%ED%8C%8C%EC%9D%B4%EC%8D%AC
