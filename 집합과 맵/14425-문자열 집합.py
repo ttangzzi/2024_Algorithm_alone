f1c60c39a4aca9e08928c298e1a5c7a5a1e0488f
ttangzzi <cvbg0802@gmail.com>
2024-06-27 17:35:33 +0900

    0627 집합과 맵 도전 실패

---
+++ 집합과 맵/14425-문자열 집합.py
@@ -0,0 +1,15 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+S = []
+test = []
+count = 0
+
+for _ in range(N):
+  S.append(sys.stdin.readline())
+
+for _ in range(M):
+  test.append(sys.stdin.readline())
+
+set_S = set(S)
+print(len(test.intersection(set_S)))
